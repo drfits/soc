d2e0a54137cfba664788e835a0c0e4d77e97832c
Evgeniy Fitsner <drfits@drfits.com>
2018-01-22 22:48:40 +0300

    fix unit-test according to time-zone

--- modules/common/test/src/test/java/com/drfits/soc/test/core/utils/StringToCalendarCompareTest.java
+++ modules/common/test/src/test/java/com/drfits/soc/test/core/utils/StringToCalendarCompareTest.java
@@ -4,23 +4,25 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 import static org.junit.Assert.assertTrue;
 
 public final class StringToCalendarCompareTest {
 
-    private final static String TEST_TIME = "2017-11-01T09:40:02.000+03:00";
     private final static String TEST_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
-
-    public StringToCalendarCompareTest() {
-    }
 
     @Test
     public void same() throws ParseException {
         final Calendar calendar = Calendar.getInstance();
+        final ZonedDateTime now = ZonedDateTime.ofInstant(calendar.toInstant(), ZoneId.systemDefault());
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TEST_TIME_FORMAT);
+        final String formatDateTime = now.format(formatter);
         final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(TEST_TIME_FORMAT);
-        calendar.setTime(simpleDateFormat.parse(TEST_TIME));
-        assertTrue(new StringToCalendarCompare(TEST_TIME).same(calendar));
+        calendar.setTime(simpleDateFormat.parse(formatDateTime));
+        assertTrue(new StringToCalendarCompare(formatDateTime).same(calendar));
     }
 }
